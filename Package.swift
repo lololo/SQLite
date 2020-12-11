99a7eb3575c535443b8a726981416e83cda272ec
lei <guolei@me.com>
2020-12-11 09:19:36 +0800

    package name

--- Package.swift
+++ Package.swift
@@ -1,15 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "SQLite.swift",
+    name: "SQLite",
     products: [.library(name: "SQLite", targets: ["SQLite"])],
     targets: [
         .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
         .target(name: "SQLiteObjc"),
         .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
-    ],
-    swiftLanguageVersions: [4, 5]
+    ]
 )
 
 #if os(Linux)
