3c09897f284aae97345e45b407859142291845d3
lei <guolei@me.com>
2021-01-28 22:32:19 +0800

    excloude info.plist

--- Package.swift
+++ Package.swift
@@ -3,21 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "SQLite",
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v10), .watchOS(.v3)],
     products: [.library(name: "SQLite", targets: ["SQLite"])],
     targets: [
-        .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
+        .target(name: "SQLite", dependencies: ["SQLiteObjc"],exclude:["Info.plist"]),
         .target(name: "SQLiteObjc"),
         .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
     ]
 )
-
-#if os(Linux)
-    package.dependencies = [.package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3")]
-    package.targets = [
-        .target(name: "SQLite", exclude: ["Extensions/FTS4.swift", "Extensions/FTS5.swift"]),
-        .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests", exclude: [
-            "FTS4Tests.swift",
-            "FTS5Tests.swift"
-        ])
-    ]
-#endif
